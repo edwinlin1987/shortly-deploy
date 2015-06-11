212e0dbc174be7f2a513955814e941e4d5454ad1
Hack Reactor Students <accounts@hackreactor.com>
2015-06-11 11:55:03 -0700

    Revert "Revert "Removed debugging tool""
    This reverts commit 816baa8587023564adf4a0d39659f754543f4b95.

--- public/client/createLinkView.js
+++ public/client/createLinkView.js
@@ -16,7 +16,6 @@ Shortly.createLinkView = Backbone.View.extend({
     e.preventDefault();
     var $form = this.$el.find('form .text');
     var link = new Shortly.Link({ url: $form.val() });
-    console.log("shortenURL,", link);
     link.on('request', this.startSpinner, this);
     link.on('sync', this.success, this);
     link.on('error', this.failure, this);
